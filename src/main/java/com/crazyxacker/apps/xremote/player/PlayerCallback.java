d210b3e1588047a583d7646a0044fb55854e4552
CrazyXacker <crazyxacker@gmail.com>
2024-07-05 13:44:21 +0300

    Remove old code

--- src/main/java/com/crazyxacker/apps/xremote/player/PlayerCallback.java
+++
@@ -1,8 +0,0 @@
-package com.crazyxacker.apps.xremote.player;
-
-import com.crazyxacker.apps.xremote.models.player.PlayerInfo;
-
-public interface PlayerCallback {
-    void onProgressUpdate(PlayerInfo playerInfo);
-    void onPlayerClose(PlayerInfo playerInfo);
-}
--- src/main/java/com/crazyxacker/apps/xremote/utils/Triple.java
+++
@@ -1,4 +0,0 @@
-package com.crazyxacker.apps.xremote.utils;
-
-public record Triple<T, U, V>(T first, U second, V third) {
-}
